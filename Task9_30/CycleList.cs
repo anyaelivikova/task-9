3746c6570e2c0776e33d800765291a09255a4d87
Anna <Anna@DESKTOP-CF9APJJ>
2019-06-21 00:24:45 +0500

    Добавьте файлы проекта.

---
+++ Task9_30/CycleList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9_30
+{
+    public class CycleList
+    {
+        private class Point
+        {
+            public int Data { get; set; }
+            public Point Next { get; set; }
+        }
+
+        Point _beg;
+        Point _last;
+
+        public void Add(int element)
+        {
+            Point p = new Point();
+            p.Data = element;
+
+            if (_beg == null)
+            {
+                _beg = p;
+                _last = p;
+                p.Next = p;
+            }
+            else
+            {
+                _last.Next = p;
+                _last = p;
+                p.Next = _beg;
+            }
+        }
+
+        public void Show()
+        {
+            Point p = _beg;
+            do
+            {
+                Console.Write(p.Data + " ");
+                p = p.Next;
+            }
+            while (p != _beg);
+
+            Console.WriteLine();
+        }
+
+        public int Sum()
+        {
+            int sum = 0;
+
+            Point p = _beg;
+            do
+            {
+                sum += p.Data;
+                p = p.Next;
+            }
+            while (p != _beg);
+
+            return sum;
+        }
+
+        public int SumRecursive()
+        {
+            return Sum(_beg.Next);
+        }
+
+        private int Sum(Point p)
+        {
+            if (p == _beg)
+                return p.Data;
+
+            return p.Data + Sum(p.Next);
+        }
+    }
+}
---
+++ Task9_30/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<<< задача 9 >>>");
+            CycleList list = new CycleList();
+
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(rand.Next(50));
+            }
+
+            list.Show();
+
+            int sum = list.Sum();
+            Console.WriteLine($"Сумма эл-тов без рекурсии = {sum}");
+            int sumRec = list.SumRecursive();
+            Console.WriteLine($"Сумма эл-тов с рекурсией = {sumRec}");
+
+            Console.ReadLine();
+        }
+    }
+}
